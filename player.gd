342668357ef504f6ab01dbc029a4b8f2748c65a0
TimmyDDthe1st <tim.douglas.1990@gmail.com>
2024-09-06 21:52:01 +0100

    sorted level end events

--- player.gd
+++ player.gd
@@ -1,31 +1,51 @@
 extends RigidBody3D
 
 var timer: float = 0.0;
+
+## Newtons of thrust applied to the ship
+@export_range(500, 2500)
 var thrust: float = 1000;
+
+@export
 var torque: float = 100;
-var gameOver: bool = false;
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
 
+var is_transitioning: bool = false;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if(!gameOver):
-		if(Input.is_action_pressed("thrust")):
-			apply_central_force(basis.y * delta * thrust)
-		if(Input.is_action_pressed("rotate_left")):
-			apply_torque(Vector3(0.0, 0.0, torque * delta))
-		if(Input.is_action_pressed("rotate_right")):
-			apply_torque(Vector3(0.0, 0.0, -torque * delta))
+func wait_for(wait_time: float, callback: Callable) -> void:
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(callback)
+
+func level_end() -> void:
+	is_transitioning = true
+	set_process(false)
+	
+func complete_level(next_level: String) -> void:
+	level_end()
+	wait_for(1.0, get_tree().change_scene_to_file.bind(next_level))
+	
+
+func game_over() -> void:
+	level_end()
+	wait_for(1.0, get_tree().reload_current_scene)
+
+func movement(delta) -> void:
+	if(Input.is_action_pressed("thrust")):
+		apply_central_force(basis.y * delta * thrust)
+	if(Input.is_action_pressed("rotate_left")):
+		apply_torque(Vector3(0.0, 0.0, torque * delta))
+	if(Input.is_action_pressed("rotate_right")):
+		apply_torque(Vector3(0.0, 0.0, -torque * delta))
 	if(Input.is_action_just_pressed("restart_level")):
 		get_tree().reload_current_scene()
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	movement(delta);
 
 func _on_body_entered(body: Node) -> void:
-	if("Goal" in body.get_groups()):
-		gameOver = true;
-		print("WIN")
-	if("Hazard" in body.get_groups()):
-		gameOver = true;
-		print("LOSE")
+	if(is_transitioning == false):
+		if("Goal" in body.get_groups()):
+			complete_level(body.file_path);
+		if("Hazard" in body.get_groups()):
+			game_over();
